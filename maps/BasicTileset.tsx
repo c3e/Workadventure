251ba1ac0069c05d0764e14c29a38f7ef6731f5b
gituser <nonono@vergisses.de>
2020-11-14 22:31:42 +0100

    Flur added

---
+++ maps/BasicTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="BasicTileset" tilewidth="32" tileheight="32" tilecount="35" columns="5">
+ <image source="../assets/tilesets/BasicTileset.png" width="166" height="232"/>
+</tileset>
