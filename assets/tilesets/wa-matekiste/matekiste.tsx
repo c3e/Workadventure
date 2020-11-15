697635a4be1fa6980e144c3ca58268db57e0ff3e
Sebi <sebi@chaospott.de>
2020-11-15 21:58:56 +0100

    Maps angepasst an: https://howto.rc3.world/maps.html (außer exitSceneUrl)

--- assets/tilesets/wa-matekiste/matekiste.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.2" name="matekiste" tilewidth="32" tileheight="32" tilecount="12" columns="4">
- <image source="matekiste.png" width="128" height="96"/>
-</tileset>
